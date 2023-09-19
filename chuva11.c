af91c6656cf6dd2da385d5f0d4d5eb1d75f90e0e
erick-ems <erick.ems@outlook.com>
2023-09-19 11:10:41 -0300

    chuva11

--- chuva11.c
+++ chuva11.c
@@ -1,57 +1,39 @@
 #include <stdio.h>
 
+// Função para definir uma matriz bidimensional
+void define_matrix(int mapa[], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            scanf("%d", &mapa[i * size + j]);
+        }
+    }
+}
 
-
-int main(){
-
-
+int main() {
     int N;
     scanf("%d", &N);
 
-
-    //Declaração de array bidimensionais para armazenar os mapas
-
+    // Declaração de arrays bidimensionais para armazenar os mapas
     int mapa1[N][N];
     int mapa2[N][N];
-
-    //Leitura dos valores do primeiro mapa
-
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            scanf("%d", &mapa1[i][j]);
-        }
-    }
-
-    //Leitura dos valores para o segundo mapa
-
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            scanf("%d", &mapa2[i][j]);
-        }
-    }
-
-
     int resultado[N][N];
 
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
+    // Leitura dos valores do primeiro mapa
+    define_matrix((int *)mapa1, N);
+    
+    // Leitura dos valores do segundo mapa
+    define_matrix((int *)mapa2, N);
+
+    // Cálculo da soma dos mapas e impressão dos resultados
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             resultado[i][j] = mapa1[i][j] + mapa2[i][j];
-        }
-    }
-
-
-    // para mostrar os resultados
-
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
             printf("%d", resultado[i][j]);
-            if(j < N - 1){
+            if (j < N - 1) {
                 printf(" ");
             }
         }
-
         printf("\n");
-
     }
 
     return 0;
